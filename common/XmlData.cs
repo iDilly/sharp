6375d9a392e95c74e5408e7b8333dbe11dbcbf0d
iDilly <41809243+iDilly@users.noreply.github.com>
2018-07-28 02:08:30 +0100

    Move XmlData into a new file.
    Moved the XmlData class from Resources.cs to a brand new file: XmlData.cs

--- common/XmlData.cs
+++ common/XmlData.cs
@@ -7,25 +7,19 @@ using System.Threading.Tasks;
 namespace common
 {
     /// <summary>
-    /// This is the Resources class, it can be seen as the parent of all game resource holders.
+    /// This is the XmlData class, it stores all the parsed XML data of the game.
     /// </summary>
-    public class Resources
+    public class XmlData
     {
         /// <summary>
         /// This is a private string variable which simply stores the root path given.
         /// </summary>
         string m_root;
 
-        /// <summary>
-        /// This public variable stores all the parsed XML data of the game, inside a class: XmlData.cs.
-        /// </summary>
-        public XmlData XmlData;
-
         /// <param name="root">Root path of your resource files.</param>
-        public Resources(string root)
+        public XmlData(string root)
         {
             m_root = root;
-            XmlData = new XmlData($"{root}/xml");
         }
     }
 }
